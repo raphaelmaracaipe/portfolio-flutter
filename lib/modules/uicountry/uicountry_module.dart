d5a7fb5c5fbb69994d5f51348da23fd351b328a4
Raphael S Maracaipe <raphaelmaracaipe@gmail.com>
2023-07-19 21:24:15 -0300

    Alterando chamada do repositório.

--- lib/modules/uicountry/uicountry_module.dart
+++ lib/modules/uicountry/uicountry_module.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:portfolio_flutter/config/env.dart';
 import 'package:portfolio_flutter/modules/core/data/assets/countries_codes.dart';
 import 'package:portfolio_flutter/modules/core/data/assets/countries_codes_impl.dart';
 import 'package:portfolio_flutter/modules/core/data/countries_repository.dart';
 import 'package:portfolio_flutter/modules/core/data/countries_repository_impl.dart';
+import 'package:portfolio_flutter/modules/core/data/network/config/network_config.dart';
+import 'package:portfolio_flutter/modules/core/data/network/rest_client.dart';
 import 'package:portfolio_flutter/modules/core/widgets/loading/loading.dart';
 import 'package:portfolio_flutter/modules/core/widgets/loading/loading_impl.dart';
 import 'package:portfolio_flutter/modules/uicountry/bloc/uicountry_bloc.dart';
@@ -18,10 +21,14 @@ class UiCountryModule extends Module {
         Bind.factory<CountriesCode>(
           (i) => CountriesCodeImpl(assetBundle: rootBundle),
         ),
-        Bind.factory<CountriesRepository>(
-          (i) => CountriesRepositoryImpl(
-            countriesCode: i(),
+        Bind<RestClient>(
+          (i) => RestClient(
+            NetworkConfig.config(),
+            baseUrl: (env?.baseUrl ?? ""),
           ),
+        ),
+        Bind.factory<CountriesRepository>(
+          (i) => CountriesRepositoryImpl(countriesCode: i()),
         ),
         Bind.factory<UICountryBloc>(
           (i) => UICountryBloc(
