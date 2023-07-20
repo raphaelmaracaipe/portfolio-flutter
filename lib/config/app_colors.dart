2aca13c0b806a9357a1ebd64698ffd1ece6941cc
Raphael S Maracaipe <raphaelmaracaipe@gmail.com>
2023-07-20 16:22:24 -0300

    Inclusão da cor de erro.

--- lib/config/app_colors.dart
+++ lib/config/app_colors.dart
@@ -4,4 +4,5 @@ class AppColors {
   static const Color colorPrimary = Color(0xFF395B85);
   static const Color colorGray = Color(0xFF79747E);
   static const Color colorSearch = Color(0xFFE9E8EF);
+  static const Color colorError = Color(0xFF8B0000);
 }
