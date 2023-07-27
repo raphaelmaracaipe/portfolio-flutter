3931cb2531eaf30ab1195a63010b7859b01874e7
Raphael S Maracaipe <raphaelmaracaipe@gmail.com>
2023-07-27 19:51:35 -0300

    Remoção de lib não usada.

--- test/modules/core/widgets/loading/loading_test.dart
+++ test/modules/core/widgets/loading/loading_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:portfolio_flutter/modules/core/localizations/app_localization.dart';
 import 'package:portfolio_flutter/modules/core/localizations/app_localization_impl.dart';
