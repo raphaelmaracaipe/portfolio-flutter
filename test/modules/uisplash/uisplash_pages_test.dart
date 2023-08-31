c8e169d5dca68ca26fa3db7557aa85c4c3436b09
Raphael S Maracaipe <raphaelmaracaipe@gmail.com>
2023-08-31 17:22:07 -0300

    Remoção de comentários.

--- test/modules/uisplash/uisplash_pages_test.dart
+++ test/modules/uisplash/uisplash_pages_test.dart
@@ -1,23 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:modular_test/modular_test.dart';
-// import 'package:portfolio_flutter/modules/uisplash/uisplash_page.dart';
-//
-// void main() {
-//   TestWidgetsFlutterBinding.ensureInitialized();
-//
-//   testWidgets('when init check if is show container', (widgetTester) async {
-//     UiSplashPage page = const UiSplashPage();
-//     await widgetTester.pumpWidget(MaterialApp(
-//       home: page,
-//     ));
-//     await widgetTester.pump(const Duration(seconds: 5));
-//
-//     Finder findContainer = find.byKey(const Key('uisplash_container'));
-//     expect(findContainer, findsOneWidget);
-//   });
-// }
-
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
