1a0bec437cc64a12e3728017b8ae16f14a842e73
Raphael S Maracaipe <raphaelmaracaipe@gmail.com>
2023-09-02 15:48:13 -0300

    Alteração da lib modular pelo o getit.

--- lib/modules/uiprofile/uiprofile_page.dart
+++ lib/modules/uiprofile/uiprofile_page.dart
@@ -1,5 +1,7 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
+@RoutePage()
 class UiProfilePage extends StatelessWidget {
   const UiProfilePage({super.key});
 
