9dc80fdeca9e9763afa9363d1adc931a524d18d1
Raphael S Maracaipe <raphaelmaracaipe@gmail.com>
2024-03-25 19:23:38 -0300

    Ajustes necessários para regex no IOS.

--- ios/Runner/regex/Regex.swift
+++ ios/Runner/regex/Regex.swift
@@ -10,20 +10,33 @@ import Foundation
 class Regex {
     
     func generate(pattern: String) -> String {
-        guard let regex = try? NSRegularExpression(pattern: pattern) else {
+        do {
+            let size = sizeExtractedOfPattern(inputString: pattern)
+            let regex = try NSRegularExpression(pattern: pattern, options: [])
+            let allowedCharacters = "0123456789ABCDEFGHJKLMNPQRSTUWXYZabcdefghijkmnopqrstuvwxyz"
+            var result = ""
+            
+            while result.isEmpty || regex.numberOfMatches(in: result, options: [], range: NSRange(location: 0, length: result.utf16.count)) == 0 {
+                result = String((0..<size).map { _ in allowedCharacters.randomElement()! })
+            }
+            
+            return result
+        } catch {
+            print("Erro ao criar expressão regular: \(error)")
             return ""
         }
-        
-        let randomString = randomStringOfLength(20)
-        let range = NSRange(location: 0, length: randomString.utf16.count)
-        
-        let generatedString = regex.stringByReplacingMatches(in: randomString, range: range, withTemplate: "")
-        return generatedString
     }
     
-    private func randomStringOfLength(_ length: Int) -> String {
-       let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-       return String((0..<length).map{ _ in letters.randomElement()! })
+    private func sizeExtractedOfPattern(inputString: String) -> Int {
+        guard let firstIndex = inputString.firstIndex(of: "{") else {
+            return 0
+        }
+        guard let lestIndex = inputString.firstIndex(of: "}") else {
+            return 0
+        }
+        
+        let textReplaced = String(inputString[firstIndex..<lestIndex]).replacingOccurrences(of: "{", with: "")
+        return Int(textReplaced) ?? 0
     }
     
 }
